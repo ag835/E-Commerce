9d8b3c6f2bbcc3f4b646d0f5e64d237903581ce7
Your Name <you@example.com>
2020-06-02 18:55:29 -0400

    WIP of register form

---
+++ public_html/RegLogin/register.php
@@ -0,0 +1,18 @@
+<form method="POST">
+    <label for="email">Email address
+    <input type="email" id="email" name="email"/>
+    </label>
+    <label for="p">Choose password
+    <input type="password" id="p" name="password"/>
+    </label>
+    <label for="cp">Re-enter password
+    <input type="password" id="cp" name="cpassword"/>
+    </label>
+    <input type="submit" name="register" value="Complete sign up"/>
+</form>
+
+<?php
+echo var_export($_GET, true);
+echo var_export($_POST, true);
+echo var_export($_REQUEST, true);
+?>
