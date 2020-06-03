382295bacbd6968cf8dace033cf871014b796f3d
Your Name <you@example.com>
2020-06-02 20:35:04 -0400

    fixed missing parantheses ?

--- public_html/RegLogin/register.php
+++ public_html/RegLogin/register.php
@@ -24,25 +24,25 @@ if(isset($_POST["register"])) {
             #echo "<div>Passwords Match</div>";
             require("config.php");
             $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
-            try{
+            try {
                 $db = new PDO($connection_string, $dbuser, $dbpass);
                 $stmt = $db->prepare("INSERT INTO Users (email, password) VALUES(:email, :password");
                 $stmt->execute(array(
-                        ":email" => $email,
-                        ":password" => $password
+                    ":email" => $email,
+                    ":password" => $password
                 ));
                 $e = $stmt->errorInfo();
-                if ($e[0] != "00000"){
+                if ($e[0] != "00000") {
                     echo var_export($e, true);
                 }
                 else {
                     echo "<div>Successfully registered</div>";
                 }
             }
-            }
-            catch (Exception $e){
+            catch (Exception $e) {
                 echo $e->getMessage();
             }
+        }
         else{
             echo "<div>Passwords do not match</div>";
         }
