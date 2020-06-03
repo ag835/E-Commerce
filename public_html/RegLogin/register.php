eb00e1ad3f52ec5ee86d7f8b08da39061c87ba1e
Your Name <you@example.com>
2020-06-02 20:23:12 -0400

    basic registration

--- public_html/RegLogin/register.php
+++ public_html/RegLogin/register.php
@@ -16,12 +16,33 @@
 #echo var_export($_POST, true);
 #echo var_export($_REQUEST, true);
 if(isset($_POST["register"])) {
-    if(isset($_POST["password"]) && isset($_POST["cpassword"])) {
+    if(isset($_POST["password"]) && isset($_POST["cpassword"]) && isset($_POST["email"])) {
         $password = $_POST["password"];
         $cpassword = $_POST["cpassword"];
+        $email = $_POST["email"];
         if($password == $cpassword) {
-            echo "<div>Passwords Match</div>";
-        }
+            #echo "<div>Passwords Match</div>";
+            require("config.php");
+            $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+            try{
+                $db = new PDO($connection_string, $dbuser, $dbpass);
+                $stmt = $db->prepare("INSERT INTO Users (email, password) VALUES(:email, :password");
+                $stmt->execute(array(
+                        ":email" => $email,
+                        ":password" => $password
+                ));
+                $e = $stmt->errorInfo();
+                if ($e[0] != "00000"){
+                    echo var_export($e, true);
+                }
+                else {
+                    echo "<div>Successfully registered</div>";
+                }
+            }
+            }
+            catch (Exception $e){
+                echo $e->getMessage();
+            }
         else{
             echo "<div>Passwords do not match</div>";
         }
