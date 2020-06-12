93f455cb869adb9c408f52c8f3bc67bb732d6460
Your Name <you@example.com>
2020-06-12 10:13:17 -0400

    ..

--- public_html/Basic/register.php
+++ public_html/Basic/register.php
@@ -47,34 +47,32 @@ if(isset($_POST["register"])) {
         $password = $_POST["password"];
         $cpassword = $_POST["cpassword"];
         $email = $_POST["email"];
-        if (!filter_var(!$email, FILTER_VALIDATE_EMAIL)) {
-            echo "<div>Please enter a valid email</div>";
-        }
-        else {
-            if ($password == $cpassword) {
-                #echo "<div>Passwords Match</div>";
-                #require("config.php");
-                $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
-                try {
-                    $db = new PDO($connection_string, $dbuser, $dbpass);
-                    $hash = password_hash($password, PASSWORD_BCRYPT);
-                    $stmt = $db->prepare("INSERT INTO Users (email, password) VALUES(:email, :password)");
-                    $stmt->execute(array(
-                        ":email" => $email,
-                        ":password" => $hash #Don't save the raw password $password
-                    ));
-                    $e = $stmt->errorInfo();
-                    if ($e[0] != "00000") {
-                        echo var_export($e, true);
-                    } else {
-                        echo "<div>Successfully registered</div>";
-                    }
-                } catch (Exception $e) {
-                    echo $e->getMessage();
+        if($password == $cpassword) {
+            #echo "<div>Passwords Match</div>";
+            #require("config.php");
+            $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+            try {
+                $db = new PDO($connection_string, $dbuser, $dbpass);
+                $hash = password_hash($password, PASSWORD_BCRYPT);
+                $stmt = $db->prepare("INSERT INTO Users (email, password) VALUES(:email, :password)");
+                $stmt->execute(array(
+                    ":email" => $email,
+                    ":password" => $hash #Don't save the raw password $password
+                ));
+                $e = $stmt->errorInfo();
+                if ($e[0] != "00000") {
+                    echo var_export($e, true);
                 }
-            } else {
-                echo "<div>Passwords do not match</div>";
+                else {
+                    echo "<div>Successfully registered</div>";
+                }
             }
+            catch (Exception $e) {
+                echo $e->getMessage();
+            }
+        }
+        else{
+            echo "<div>Passwords do not match</div>";
         }
     }
 }
