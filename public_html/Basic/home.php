28c259bf6ec0a13cb8738dfbe3a18622d39f6dfa
Your Name <you@example.com>
2020-06-12 17:54:14 -0400

    bg and form

--- public_html/Basic/home.php
+++ public_html/Basic/home.php
@@ -3,6 +3,11 @@ include("header.php");
 
 ?>
 <h4>Home</h4>
+<style>
+    body {
+        background-image: url('bigbang.jpg');
+    }
+</style>
 
 <?php
 echo "Hello, " . $_SESSION["user"]["email"];
