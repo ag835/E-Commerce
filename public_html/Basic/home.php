9209fccc76011f3b927a644eb85292ec7da3cd6e
Your Name <you@example.com>
2020-06-12 19:03:32 -0400

    undid that, changed bg img

--- public_html/Basic/home.php
+++ public_html/Basic/home.php
@@ -5,7 +5,7 @@ include("header.php");
 <h4>Home</h4>
 <style>
     body {
-        background-image: url('bluelight.jpg');
+        background-image: url('signs.jpg');
         background-repeat: no-repeat;
         background-size: cover;
     }
