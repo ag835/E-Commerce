911d8e848a38ae0f147f483296812b185e08ac00
Your Name <you@example.com>
2020-06-17 13:39:01 -0400

    (current_date) and copied files

--- public_html/SQLTables/productsTable.sql
+++ public_html/SQLTables/productsTable.sql
@@ -5,7 +5,7 @@ CREATE TABLE Products (
     Quantity int,
     Price decimal(6,2) DEFAULT 0.00,
     Description TEXT,
-    Release_Date date DEFAULT CURRENT_DATE,
+    Release_Date date DEFAULT (CURRENT_DATE),
     Modified datetime default current_timestamp on update current_timestamp,
     Created datetime default current_timestamp,
     PRIMARY KEY(ID)
