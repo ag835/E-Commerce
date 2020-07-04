0d47b01fc1ead193368e19c990afcb089a62381a
Your Name <you@example.com>
2020-07-04 08:45:05 -0400

    trying to reference foreign users id

--- public_html/Interm/Queries/insert_into_cart.sql
+++ public_html/Interm/Queries/insert_into_cart.sql
@@ -1,4 +1,4 @@
-INSERT INTO Carts (productID, userID) VALUES (:productID, :id)
+INSERT INTO Carts (productID, userID) VALUES (:productID, Users(:id))
 UPDATE Carts SET quantity = quantity + 1 WHERE productID = :productId --not sure if this will work bc columns..
 /*//something like this, otherwise try products(id) or products.id or whatever it is
 //also user is logged in so how would I get user id?
