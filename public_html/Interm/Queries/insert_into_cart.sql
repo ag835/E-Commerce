5119f55b25897d863a487084761822f1caccb293
Your Name <you@example.com>
2020-07-04 08:33:25 -0400

    testing add to carts table

--- public_html/Interm/Queries/insert_into_cart.sql
+++ public_html/Interm/Queries/insert_into_cart.sql
@@ -1,5 +1,5 @@
-INSERT INTO Carts (productID, userID) VALUES (:id, :userid)
-UPDATE Carts SET quantity = quantity + 1 WHERE productID = :id //not sure if this will work bc columns..
-//something like this, otherwise try products(id) or products.id or whatever it is
+INSERT INTO Carts (productID, userID) VALUES (:productID, :id)
+UPDATE Carts SET quantity = quantity + 1 WHERE productID = :productId --not sure if this will work bc columns..
+/*//something like this, otherwise try products(id) or products.id or whatever it is
 //also user is logged in so how would I get user id?
 //check login.php and logout.php...
