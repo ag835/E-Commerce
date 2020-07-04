14118abf9f1371382a9ea551f0ccf8969ac44bd4
Your Name <you@example.com>
2020-07-04 08:48:41 -0400

    removed userid for a sec

--- public_html/Interm/Queries/insert_into_cart.sql
+++ public_html/Interm/Queries/insert_into_cart.sql
@@ -1,5 +1,5 @@
-INSERT INTO Carts (productID, userID) VALUES (:productID, Users(:id))
-UPDATE Carts SET quantity = quantity + 1 WHERE productID = :productId --not sure if this will work bc columns..
+INSERT INTO Carts (productID) VALUES (:productID)
+--UPDATE Carts SET quantity = quantity + 1 WHERE productID = :productId --not sure if this will work bc columns..
 /*//something like this, otherwise try products(id) or products.id or whatever it is
 //also user is logged in so how would I get user id?
 //check login.php and logout.php...
