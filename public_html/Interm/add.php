6c3d316fe9e6fdb25dd1975eddab427d20a8906c
Your Name <you@example.com>
2020-07-04 15:10:09 -0400

    uncommented common.inc.php

--- public_html/Interm/add.php
+++ public_html/Interm/add.php
@@ -5,12 +5,12 @@ if (isset($_GET["productId"]) && !empty($_GET["productId"])){
         $userId = $_SESSION["user"]["id"];
         $query = file_get_contents(__DIR__ . "/Queries/insert_into_cart.sql");
         if(isset($query) && !empty($query)) {
-            #require("common.inc.php");
+            require("common.inc.php");
             $stmt = getDB()->prepare($query);
             $stmt->execute([":productID"=>$productId], [":userID"=>$userId]); //:id -> :productID, added user id stuff
             $e = $stmt->errorInfo();
             if($e[0] == "00000"){
-                echo "Successfully added to cart.";
+                #echo "Successfully added to cart.";
                 die(header("Location: store.php"));
             }
             else{
