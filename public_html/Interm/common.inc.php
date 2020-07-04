fafa754b17166340ab4b869aec4e608beca04763
Your Name <you@example.com>
2020-07-04 15:28:03 -0400

    added DIR

--- public_html/Interm/common.inc.php
+++ public_html/Interm/common.inc.php
@@ -8,7 +8,7 @@ function get($arr, $key){
 function getDB(){
     global $db;
     if(!isset($db)) {
-        require("config.php");
+        require(__DIR__ . "config.php");
         $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
         $db = new PDO($connection_string, $dbuser, $dbpass);
     }
