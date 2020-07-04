be00f4fcdee44c76e5eb2c6d318a4fa01b04f178
Your Name <you@example.com>
2020-07-04 13:39:26 -0400

    test

--- public_html/Interm/Queries/insert_into_cart.sql
+++ public_html/Interm/Queries/insert_into_cart.sql
@@ -1,6 +1,5 @@
 INSERT INTO Carts (productID, userID) VALUES (:productID, Users.id); --doesn't work
---SELECT * FROM (SELECT productID)
---WHERE NOT EXISTS (SELECT productID FROM Carts WHERE productID = productID) LIMIT 1;
+--create unique key for productID and userID? or try IGNORE, or something like that
 UPDATE Carts
    SET quantity = quantity + 1
  WHERE productID = :productID
