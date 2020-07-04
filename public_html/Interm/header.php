d02d7d58d03fba986877016463c6b856d1436631
Your Name <you@example.com>
2020-07-04 10:44:00 -0400

    added store to header and header to store.php

--- public_html/Interm/header.php
+++ public_html/Interm/header.php
@@ -9,6 +9,7 @@ session_start();
 <nav
     <ul>
         <li><a href="home.php">Home</a></li>
+        <li><a href="store.php">Store</a></li>
         <li><a href="register.php">Register</a></li>
         <li><a href="login.php">Login</a></li>
         <li><a href="logout.php">Logout</a></li>
