44a61bda96275805edaa355e6fc644008d21c8bd
Your Name <you@example.com>
2020-07-04 16:12:52 -0400

    fixed array

--- public_html/Interm/add.php
+++ public_html/Interm/add.php
@@ -7,7 +7,7 @@ if (isset($_GET["productId"]) && !empty($_GET["productId"])){
         if(isset($query) && !empty($query)) {
             require("common.inc.php");
             $stmt = getDB()->prepare($query);
-            $stmt->execute([":productID"=>$productId], [":userID"=>$userId]); //:id -> :productID, added user id stuff
+            $stmt->execute([":productID"=>$productId, ":userID"=>$userId]); //:id -> :productID, added user id stuff
             $e = $stmt->errorInfo();
             if($e[0] == "00000"){
                 #echo "Successfully added to cart.";
