f9fde432e712f0afda360dcf8118fd492651b441
Your Name <you@example.com>
2020-07-04 14:11:24 -0400

    config fixes and :userID

--- public_html/Interm/Queries/insert_into_cart.sql
+++ public_html/Interm/Queries/insert_into_cart.sql
@@ -1,4 +1,4 @@
-INSERT INTO Carts (productID, userID) VALUES (:productID, Users.id); --doesn't work
+INSERT INTO Carts (productID, userID) VALUES (:productID, :userID);
 --create unique key for productID and userID? or try IGNORE, or something like that
 UPDATE Carts
    SET quantity = quantity + 1
