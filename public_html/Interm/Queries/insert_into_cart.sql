fbcb04b78f853cdd5e018ae1f43bffee4795411e
Your Name <you@example.com>
2020-07-05 08:56:19 -0400

    removed userid....

--- public_html/Interm/Queries/insert_into_cart.sql
+++ public_html/Interm/Queries/insert_into_cart.sql
@@ -1,5 +1,4 @@
-INSERT INTO Carts (productID, userID) VALUES (:productID, :userID);
---create unique key for productID and userID? or try IGNORE, or something like that
+INSERT INTO Carts (productID) VALUES (:productID);
 UPDATE Carts
    SET quantity = quantity + 1
  WHERE productID = :productID
