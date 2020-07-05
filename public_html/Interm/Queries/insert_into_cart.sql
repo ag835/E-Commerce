1c7f1aeed80ff3e8c5c3283e68b001d29a5de58b
Your Name <you@example.com>
2020-07-05 14:58:02 -0400

    unqiue index

--- public_html/Interm/Queries/insert_into_cart.sql
+++ public_html/Interm/Queries/insert_into_cart.sql
@@ -1,4 +1,4 @@
-INSERT INTO Carts (productID, userID) VALUES (:productID, :userID);
+INSERT IGNORE INTO Carts (productID, userID) VALUES (:productID, :userID);
 UPDATE Carts
    SET quantity = quantity + 1
- WHERE productID = :productID
+ WHERE productID = :productID AND userID = :userID
