f52e5b7d646a1ba66666d1f509617a213c800e8a
Your Name <you@example.com>
2020-07-05 18:32:13 -0400

    how long was that ; missing

--- public_html/Interm/Queries/select_cart.sql
+++ public_html/Interm/Queries/select_cart.sql
@@ -1,1 +1,6 @@
 SELECT * FROM Products INNER JOIN Carts ON (Products.id = Carts.productID)
+/*
+SELECT Carts.id, Products.name, Products.quantity, Products.price
+FROM Products, Carts
+WHERE Products.id = Carts.productID AND Carts.userID = :userID
+ */
