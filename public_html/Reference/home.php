f8935b8ce15a71a0ab46a7c617809353d8b98b25
Your Name <you@example.com>
2020-07-17 21:15:05 -0400

    removed irrelevant code

--- public_html/Reference/home.php
+++ public_html/Reference/home.php
@@ -4,7 +4,7 @@ include_once(__DIR__."/partials/header.partial.php");
 if(Common::is_logged_in()){
     //this will auto redirect if user isn't logged in
 
-    Common::aggregate_stats_and_refresh();
+    //Common::aggregate_stats_and_refresh();
 }
 $last_updated = Common::get($_SESSION, "last_sync", false);
 ?>
