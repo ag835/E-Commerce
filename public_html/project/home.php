50728a8112445190a40162c428e4a13aa61f28d4
Your Name <you@example.com>
2020-07-18 09:43:45 -0400

    lets try this again

---
+++ public_html/project/home.php
@@ -0,0 +1,37 @@
+<?php
+include_once(__DIR__."/partials/header.partial.php");
+
+if(Common::is_logged_in()){
+    //this will auto redirect if user isn't logged in
+
+    Common::aggregate_stats_and_refresh();
+}
+$last_updated = Common::get($_SESSION, "last_sync", false);
+?>
+<div class="container-fluid">
+    <h4>Home</h4>
+    <p>Welcome, <?php echo Common::get_username();?></p>
+    <?php if($last_updated):?>
+        <p>Stats Last Updated: <?php echo $last_updated->format('Y-m-d H:i:s');;?></p>
+    <?php endif;?>
+    <table class="table">
+        <thread>
+            <tr>
+                <td>Level</td>
+                <td>Experience</td>
+                <td>Points</td>
+                <td>Wins</td>
+                <td>Losses</td>
+            </tr>
+        </thread>
+        <tbody>
+        <tr>
+            <td><?php echo Common::get($_SESSION["user"], "level", 0);?></td>
+            <td><?php echo Common::get($_SESSION["user"], "experience", 0);?></td>
+            <td><?php echo Common::get($_SESSION["user"], "points", 0);?></td>
+            <td><?php echo Common::get($_SESSION["user"], "wins", 0);?></td>
+            <td><?php echo Common::get($_SESSION["user"], "losses", 0);?></td>
+        </tr>
+        </tbody>
+    </table>
+</div>
