1a03da181c55efd4e2e74faaff1bdf227d7052ff
Your Name <you@example.com>
2020-07-17 21:23:43 -0400

    .

--- public_html/Reference/home.php
+++ public_html/Reference/home.php
@@ -1,7 +1,7 @@
 <?php
 include_once(__DIR__."/partials/header.partial.php");
 
-if(Common::is_logged_in()){
+//if(Common::is_logged_in()){
     //this will auto redirect if user isn't logged in
 
     //Common::aggregate_stats_and_refresh();
