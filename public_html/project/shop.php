0a6432900ffccf98efffa6b0f874494a38f64701
Your Name <you@example.com>
2020-07-21 11:03:01 -0400

    css brand color

--- public_html/project/shop.php
+++ public_html/project/shop.php
@@ -17,9 +17,9 @@ if(Common::is_logged_in()){
 $last_updated = Common::get($_SESSION, "last_sync", false);
 ?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark justify-content-between">
-    <a class="navbar-brand">Store</a>
+    <a class="navbar-brand" style="FFFFFF">Store</a>
     <form class="form-inline">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search the store" aria-label="Search">
+        <input class="form-control mr-sm-2" type="search" placeholder="search the store" aria-label="Search">
         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search the store</button>
     </form>
 </nav>
