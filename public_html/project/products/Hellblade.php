af21ecf424ab76d9889ad69b78b13565b5fc96fc
Your Name <you@example.com>
2020-07-24 18:39:56 -0400

    font size, video

--- public_html/project/products/Hellblade.php
+++ public_html/project/products/Hellblade.php
@@ -4,26 +4,27 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 include_once(__DIR__."/../partials/header.partial.php");
 $item = "Hellblade";
-echo var_export($item);
+//echo var_export($item);
 if(Common::is_logged_in()){
     //this will auto redirect if user isn't logged in
     $result = DBH::get_item($item);
-    echo var_export($item);
+    //echo var_export($item);
     $_item = Common::get($result, "data", false);
     if($_item){
         $item = $_item;
-        echo var_export($item);
+        //echo var_export($item);
         //echo var_export($items);
     }
-    echo var_export($item);
+    //echo var_export($item);
 }
 ?>
 <button class="btn btn-sm btn-secondary">Back to store</button>
 <hr>
-<h1><?php echo Common::get($item,"name"); ?><small>(<?php echo Common::get($item,"category");?>)</small> </h1>
+<h1><?php echo Common::get($item,"name");?> <span
+            style="font-size: 50%; ">(<?php echo Common::get($item,"category");?>)</span></h1>
 <h5>Release date: <?php echo Common::get($item,"Release_Date");?></h5>
-<p>[video/images]</p>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/PRQgOfb9EGc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 <p><?php echo Common::get($item,"description");?></p>
-<div class="inline"><h4>$<?php echo Common::get($item,"price"); ?></h4><button class="btn btn-success">Add to cart</button></div>
+<div class="inline"><h4 class="inline">$<?php echo Common::get($item,"price"); ?></h4><button class="btn btn-success inline">Add to cart</button></div>
 <h3>Reviews</h3>
 <hr>
