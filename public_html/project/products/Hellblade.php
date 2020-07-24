936c7f49aad3963c26e332d3fb022f16ac9721a5
Your Name <you@example.com>
2020-07-24 18:49:06 -0400

    ..

--- public_html/project/products/Hellblade.php
+++ public_html/project/products/Hellblade.php
@@ -18,13 +18,16 @@ if(Common::is_logged_in()){
     //echo var_export($item);
 }
 ?>
+<br>
 <button class="btn btn-sm btn-secondary">Back to store</button>
-<hr>
 <h1><?php echo Common::get($item,"name");?> <span
             style="font-size: 50%; ">(<?php echo Common::get($item,"category");?>)</span></h1>
 <h5>Release date: <?php echo Common::get($item,"Release_Date");?></h5>
 <iframe width="560" height="315" src="https://www.youtube.com/embed/PRQgOfb9EGc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 <p><?php echo Common::get($item,"description");?></p>
-<div class="inline"><h4 class="inline">$<?php echo Common::get($item,"price"); ?></h4><button class="btn btn-success inline">Add to cart</button></div>
+<div><h4 class="inline">$<?php echo Common::get($item,"price"); ?></h4><button class="btn btn-success inline">Add to cart</button></div>
+<span>$<?php echo Common::get($item,"price");?></span>
+<button class="btn btn-success inline">Add to cart</button>
+<br>
 <h3>Reviews</h3>
 <hr>
