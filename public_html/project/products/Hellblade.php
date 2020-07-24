8adf762b1940541ffc47259279aff8800183a1f3
Your Name <you@example.com>
2020-07-24 18:22:53 -0400

    debugging

--- public_html/project/products/Hellblade.php
+++ public_html/project/products/Hellblade.php
@@ -4,14 +4,18 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 include_once(__DIR__."/../partials/header.partial.php");
 $item = "Hellblade";
+echo var_export($item);
 if(Common::is_logged_in()){
     //this will auto redirect if user isn't logged in
     $result = DBH::get_item($item);
+    echo var_export($item);
     $_item = Common::get($result, "data", false);
     if($_item){
         $item = $_item;
+        echo var_export($item);
         //echo var_export($items);
     }
+    echo var_export($item);
 }
 ?>
 <button class="btn btn-sm btn-secondary">Back to store</button>
