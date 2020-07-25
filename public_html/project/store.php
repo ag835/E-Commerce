82c51ec1b6ac56bedb341b1513226c1a8e3d07b4
Your Name <you@example.com>
2020-07-25 13:26:21 -0400

    added data-quantity to input

--- public_html/project/store.php
+++ public_html/project/store.php
@@ -125,7 +125,7 @@ $last_updated = Common::get($_SESSION, "last_sync", false);
         if(!updated){
             let $li = $("<li></li>");
             $li.attr("class", "list-group-item");
-            $li.append("<span></span><input class='cart-quantity-input' type='number' " +
+            $li.append("<span></span><input class='cart-quantity-input' data-quantity='quantity' type='number' " +
                 "name='item_quantity' id='item_quantity' min='1' value='1'/>");
             $li.append("<button onclick='removeFromCart(this);' class='btn btn-sm btn-danger'>X</button>");
             let itemQuantity = $("#item_quantity").data("quantity");
