074c14837d9879cbade09993bbe31d2f033589a6
Your Name <you@example.com>
2020-07-26 09:37:08 -0400

    test

--- public_html/project/store.php
+++ public_html/project/store.php
@@ -118,7 +118,7 @@ $last_updated = Common::get($_SESSION, "last_sync", false);
                 let q = $("#item_quantity").data("quantity");
                 q++;
                 $(item).data("quantity", q);
-                $(item).find("span").text(_itemName + ": ");// + q);
+                $(item).find("span").text(_itemName + ": "+ q);
                 updated = true;
             }
         });
@@ -128,12 +128,12 @@ $last_updated = Common::get($_SESSION, "last_sync", false);
             $li.append("<span></span><input class='cart-quantity-input'  type='number' " +
                 "name='item_quantity' id='item_quantity' min='1' data-quantity value='1'/>");
             $li.append("<button onclick='removeFromCart(this);' class='btn btn-sm btn-danger'>X</button>");
-            let itemQuantity = $("#item_quantity").data("quantity"); //test
+            let itemQuantity = $("#item_quantity").data("quantity");
             $li.data("quantity", itemQuantity);
             $li.data("price", itemPrice);
             $li.data("name", itemName);
             $li.data("id", itemId);
-            $li.find("span").text(itemName + ": "); //+ 1);
+            $li.find("span").text(itemName + ": "+ 1);
 
             $cart.append($li);
         }
