be126fa36de6ea0aa591c2c44ac88028c0a145e8
Your Name <you@example.com>
2020-07-26 11:10:22 -0400

    ..

--- public_html/project/store.php
+++ public_html/project/store.php
@@ -130,7 +130,7 @@ $last_updated = Common::get($_SESSION, "last_sync", false);
             $li.attr("class", "list-group-item");
             //$li.append("<span></span><input class='cart-quantity-input'  type='number' " +
                 //"name='item_quantity' id='item_quantity' min='1' data-quantity value='1'/>");
-            $li.append("<button onclick='removeFromCart(this);' class='btn btn-sm btn-danger'>X</button>");
+            $li.append("<span></span><button onclick='removeFromCart(this);' class='btn btn-sm btn-danger'>X</button>");
             //let itemQuantity = $("#item_quantity").data("quantity");
             $li.data("quantity", 1);
             $li.data("price", itemPrice);
