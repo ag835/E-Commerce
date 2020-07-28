2101c8dbcff771341953680c6f7248974d982bec
Your Name <you@example.com>
2020-07-28 18:35:03 -0400

    fixed file path

--- public_html/project/item_details.php
+++ public_html/project/item_details.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-include_once(__DIR__."/../partials/header.partial.php");
+include_once(__DIR__."/partials/header.partial.php");
 $item = [];
 if(Common::is_logged_in()){
     //this will auto redirect if user isn't logged in
