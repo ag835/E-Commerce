f8025cd1c751bcf6d64cedf58682b9a7081c23fe
Your Name <you@example.com>
2020-08-02 11:28:33 -0400

    removed login restriction

--- public_html/project/item_details.php
+++ public_html/project/item_details.php
@@ -10,7 +10,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);*/
 include_once(__DIR__."/partials/header.partial.php");
 $item = [];
-if(Common::is_logged_in()){
+//if(Common::is_logged_in()){
     //this will auto redirect if user isn't logged in
     if(isset($_GET["p"])){
         $product_id = $_GET["p"];
@@ -20,7 +20,7 @@ if(Common::is_logged_in()){
     if($_item){
         $item = $_item;
     }
-}
+//}
 ?>
 <style>
     body {
